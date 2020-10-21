1733904dc59eb5fb4b47ed6193e36749eba2d2e9
Kenneth Gilmore <gilmorekenneth@gmail.com>
2020-10-21 19:33:46 -0400

    fixed db

--- db/migrate/20200409222706_create_flights.rb
+++ db/migrate/20200409222706_create_flights.rb
@@ -2,8 +2,10 @@ class CreateFlights < ActiveRecord::Migration[5.2]
   def change
     create_table :flights do |t|
       t.datetime :date
+      t.string :airline
       t.integer :duration
       t.integer :price
+      t.integer :layovers
       t.references :from_airport
       t.references :to_airport
 
